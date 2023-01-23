78c2b9c9c34bbdf0a79162b05fee082676dbced4
Ddhruv-IOT <ddhruvarora2@gmail.com>
2023-01-23 23:07:51 +0530

    frontend files

---
+++ client/src/index.js
@@ -0,0 +1,10 @@
+import 'mdb-react-ui-kit/dist/css/mdb.min.css';
+import "@fortawesome/fontawesome-free/css/all.min.css";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+    <App />
+);
