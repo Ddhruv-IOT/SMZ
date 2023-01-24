4a903f2b80ec0b6942c32de2d27c2b068da91b6f
Ddhruv-IOT <ddhruvarora2@gmail.com>
2023-01-24 13:49:26 +0530

    updating styles

--- client/src/pages/test.js
+++ client/src/pages/test.js
@@ -4,6 +4,12 @@ import {useNavigate} from "react-router-dom"
 import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
+import "./test.css"
+import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
+import Card from 'react-bootstrap/Card';
+import FloatingLabel from 'react-bootstrap/FloatingLabel';
+
 
 function Register1() {
 
@@ -21,15 +27,105 @@ function Register1() {
     }
   
     return (
-     
-    <Container>
-      <Row>
-        <Col lg={2} md={5}>1 of 2</Col>
-        <Col>2 of 2</Col>
-      </Row>
-    </Container>
-  
-        
+    //  <div style={{background:'red', height:"100vh", alignItems:'center', justifyContent:'center', alignContent:'center'}}>
+        <Container fluid style={{display:'flex',height:'auto', alignItems:'center', justifyContent:'center', padding:"20px", background:'skyblue', justifyItems: 'center'}}  className="center">
+            <Card style={{ width: '90vw', borderRadius:"25px", alignSelf: 'center',justifySelf:'center', padding:'35px 10px'}} className="text-center">
+                <Card.Body>
+                    <Row className="d-flex justify-content-center">
+                        <Col lg={2} md={5} className="color">
+                            <h3 className="fw-bold mb-5 mt-4"> Sign Up </h3>
+                            <Row className="d-flex justify-content-center mb-4">
+                                <div className='ctr'>
+                                    <Col md>
+                                        <FloatingLabel
+                                            controlId="floatingInput"
+                                            label="Email"
+                                            className="mb-3">
+                                                <Form.Control type="email" placeholder="name@example.com"/>
+                                        </FloatingLabel>
+                                    </Col>
+
+                                    <Col md>
+                                        <FloatingLabel
+                                            controlId="floatingInput"
+                                            label="Password"
+                                            className="mb-3">
+                                                <Form.Control type="password" placeholder="name@example.com" />
+                                        </FloatingLabel>
+                                    </Col>
+
+                                    <Col md>
+                                        <FloatingLabel
+                                            controlId="floatingInput"
+                                            label="Your Email"
+                                            className="mb-3">
+                                                <Form.Control type="email" placeholder="name@example.com" />
+                                        </FloatingLabel>
+                                    </Col>
+
+                                    <Col md>
+                                        <FloatingLabel
+                                            controlId="floatingInput"
+                                            label="Your Email"
+                                            className="mb-3">
+                                                <Form.Control type="email" placeholder="name@example.com" />
+                                        </FloatingLabel>
+                                    </Col>
+
+                                    <Col md>
+                                        <FloatingLabel
+                                            controlId="floatingInput"
+                                            label="Your Email"
+                                            className="mb-3">
+                                                <Form.Control type="email" placeholder="name@example.com" />
+                                        </FloatingLabel>
+                                    </Col>
+
+                                    <Col md>
+                                        <FloatingLabel
+                                            controlId="floatingInput"
+                                            label="Your Email"
+                                            className="mb-3">
+                                                <Form.Control type="email" placeholder="name@example.com" />
+                                        </FloatingLabel>
+                                    </Col>
+
+                                    <Col md>
+                                        <FloatingLabel
+                                            controlId="floatingInput"
+                                            label="Your Email"
+                                            className="mb-3">
+                                                <Form.Control type="email" placeholder="name@example.com" />
+                                        </FloatingLabel>
+                                    </Col>
+
+                                    <Col md>
+                                        <FloatingLabel
+                                            controlId="floatingInput"
+                                            label="Your Email"
+                                            className="mb-3">
+                                                <Form.Control type="email" placeholder="name@example.com" />
+                                        </FloatingLabel>
+                                    </Col>
+
+                                    <Col md>
+                                        <FloatingLabel
+                                            controlId="floatingInput"
+                                            label="Your Email"
+                                            className="mb-3">
+                                                <Form.Control type="email" placeholder="name@example.com" />
+                                        </FloatingLabel>
+                                    </Col>
+
+                                </div>
+                            </Row>
+                        </Col>
+                        <Col>2 of 2</Col>
+                    </Row>
+                </Card.Body>
+            </Card>
+        </Container>
+    // </div>
     );
   }
   
